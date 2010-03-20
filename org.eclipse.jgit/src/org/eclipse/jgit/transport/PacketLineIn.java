6af99c906a0a43c780034d690c220989a83f55de
saces <saces@freenetproject.org>
2010-03-20 18:30:29 +0100

    visibility fixes

--- org.eclipse.jgit/src/org/eclipse/jgit/transport/PacketLineIn.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/transport/PacketLineIn.java
@@ -54,7 +54,7 @@ import org.eclipse.jgit.lib.MutableObjectId;
 import org.eclipse.jgit.util.IO;
 import org.eclipse.jgit.util.RawParseUtils;
 
-class PacketLineIn {
+public class PacketLineIn {
 	static final String END = new String("") /* must not string pool */;
 
 	static enum AckNackResult {
@@ -74,7 +74,7 @@ class PacketLineIn {
 
 	private final byte[] lineBuffer;
 
-	PacketLineIn(final InputStream i) {
+	public PacketLineIn(final InputStream i) {
 		in = i;
 		lineBuffer = new byte[SideBandOutputStream.SMALL_BUF];
 	}
@@ -117,7 +117,7 @@ class PacketLineIn {
 		return RawParseUtils.decode(Constants.CHARSET, raw, 0, len);
 	}
 
-	String readStringRaw() throws IOException {
+	public String readStringRaw() throws IOException {
 		int len = readLength();
 		if (len == 0)
 			return END;
